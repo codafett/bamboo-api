3bb529fffe8d3096548eaadc1f24f18238915136
Reuben Greaves <reuben.greaves@leasle.com>
2021-03-09 21:50:19 +0000

    Added most functionality

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 var restify = require('restify');
+var socketio = require('socket.io')(80);
 const { default: configureDb } = require('./src/db/db');
-const { Product } = require('./src/models/models');
 const { default: createLogger } = require('./src/utils/logger/Logger');
 
 const logger = createLogger('server');
 
-function respond(req, res, next) {
-  res.send('hello ' + req.params.name);
-  next();
-}
-
 var server = restify.createServer();
+var io = socketio.listen(server.server);
 
 // Set up db
 configureDb();
@@ -20,15 +16,30 @@ const { default: ProductRouter } = require('./src/routes/ProductRouter');
  
 const cors = corsMiddleware({
   preflightMaxAge: 5, //Optional
-  origins: [/^https?:\/\/localhost(:[\d]+)?$/],
+  origins: ['http://localhost:3001', 'localhost:3001', 'http://localhost:3001/'], // [/^https?:\/\/localhost(:[\d]+)?$/, 'http://localhost:3001'],
   allowHeaders: ['API-Token'],
-  exposeHeaders: ['API-Token-Expiry']
+  exposeHeaders: ['API-Token-Expiry', 'Access-Control-Allow-Origin'],
 });
- 
+
 server.pre(cors.preflight);
 server.use(cors.actual);
 
+server.use(restify.plugins.queryParser({
+  mapParams: true
+ }));
+ server.use(restify.plugins.bodyParser({
+ mapParams: true
+  }));
+ server.use(restify.plugins.acceptParser(server.acceptable));
+
 ProductRouter(server)
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
 
 server.listen(3000, function() {
   logger.log('%s listening at %s', server.name, server.url);
--- src/routes/ProductRouter.js
+++ src/routes/ProductRouter.js
@@ -1,22 +1,30 @@
-// var express = require('express');
-// var router = express.Router();
-
 import { Product } from '../models/models';
 import { wrap } from './routeUtils';
-
-// /* GET home page. */
-// router.get('/products', async function(_req, res) {
-//   res.send(await Product.find());
-// });
-
-// module.exports = router;
 
 const ProductRouter = (server) => {
   server.get('/products', wrap(async function(_req, res) {
     res.send(await Product.find());
   }));  
-  server.get('/product/:productId', wrap(async function(req, res) {
+  server.get('/products/:productId', wrap(async function(req, res) {
     res.send(await Product.findById(req.params.productId));
+  }));
+  server.put('/products', wrap(async function(req, res) {
+    const newProduct = await Product.create({
+      name: req.body.name,
+      price: req.body.price,
+    });
+    res.send(newProduct);
+  }));  
+  server.put('/products/:productId/comment', wrap(async function(req, res) {
+    const product = await Product.findById(req.params.productId);
+    if (product) {
+      product.comments.push({
+        comment: req.body.comment,
+        username: 'user1',
+      });
+      await product.save();
+    }
+    res.send(product.comments[product.comments.length - 1]);
   }));  
 }
 
